898a7e51f2c442da18d87eadaf7f284b7fe9ff0d
Binh Nguyen <tug21976@temple.edu>
2018-11-15 22:37:04 -0500

    Initial commit
    Include provided template and .gitignore for data files.

---
+++ hw3.py
@@ -0,0 +1,32 @@
+def hinge_loss(train_y, pred_y):
+	return None
+	
+def squared_loss(train_y, pred_y):
+	return None
+	
+def logistic_loss(train_y, pred_y):
+	return None
+	
+def l1_reg(w):
+	return None
+	
+def l2_reg(w):
+	return None	
+
+def train_classifier(train_x, train_y, learn_rate, loss, lambda_val, regularizer):
+	return None
+
+def test_classifier(w, test_x):
+	return None
+
+def compute_accuracy(test_y, pred_y):
+	return None
+
+def get_id():
+	return None
+
+def main():
+	return None
+
+if __name__ == "__main__":
+    main()
