649f0fbf986a216d1272498088f86a5bddee6a95
Binh Nguyen <tug21976@temple.edu>
2018-11-18 13:13:33 -0500

    Fix bugs in previous commits

--- hw3.py
+++ hw3.py
@@ -10,12 +10,12 @@ def hinge_loss(train_y, pred_y):
 
 # Calculate squared loss given training and prediction label vectors.
 def squared_loss(train_y, pred_y):
-    return np.sum((train_y-pred_y)**2)
+    return np.sum((1-train_y*pred_y)**2)
 
 
 # Calculate logistic loss given training and prediction label vectors.
 def logistic_loss(train_y, pred_y):
-    return np.log(1+np.exp(-train_y*pred_y).sum() / math.log(2))
+    return np.log(1+np.exp(-train_y*pred_y)).sum() / math.log(2)
 
 
 # Calculate the value of L1 regularizer given vector of linear classifer weights.
@@ -32,19 +32,18 @@ def l2_reg(w):
 # learning rate, loss function, lambda tradeoff parameter, and regularizer.
 def train_classifier(train_x, train_y, learn_rate, loss, lambda_val, regularizer):
     train_x_padded = padding(train_x)
-    weights = np.dot(pseudo_inverse(train_x_padded), train_y)
-    gradient = np.ones(weights.size)
+    weights = 0.2 * np.random.randn(train_x_padded.shape[1])
+    gradient = num_gradient(calc_loss_with_reg, weights, loss, lambda_val, regularizer, train_x, train_y)
     while not np.allclose(gradient, np.zeros(weights.size), atol=1e-12):
-        gradient = num_gradient(calc_loss_with_reg, weights,
-                                loss, regularizer, train_x, train_y)
         weights = weights - learn_rate*gradient
+        gradient = num_gradient(calc_loss_with_reg, weights, loss, lambda_val, regularizer, train_x, train_y)
     return weights
 
 
 # Return prediction label vectors given vector of learned classifier weights
 # and test examples.
 def test_classifier(w, test_x):
-    activation = np.dot(padding(test_x), w).reshape((test_x.shape[0], 1))
+    activation = test_regressor(w, test_x).reshape((test_x.shape[0], 1))
     return np.apply_along_axis(lambda y: 1 if y > 0 else -1, 1, activation)
 
 
@@ -64,21 +63,22 @@ def padding(X):
     return np.append(X, np.ones((X.shape[0], 1)), 1)
 
 
-# Calculate the pseudo-inverse of a matrix.
-def pseudo_inverse(matrix):
-    return np.dot(np.linalg.inv(np.dot(matrix.T, matrix)), matrix.T)
+# Return regression vector (or activations for classification) given venctor of
+# learned weights and test examples.
+def test_regressor(w, test_x):
+    return np.dot(padding(test_x), w)
 
 
 # Calculate loss given vector of linear classifier weights, loss function, and
 # training data.
 def calculate_loss(w, func, train_x, train_y):
-    return func(train_y, test_classifier(w, train_x))
+    return func(train_y, test_regressor(w, train_x))
 
 
 # Calculate loss with regularization given vector or linear classifier weights,
 # loss function, regularizer, and training data.
-def calc_loss_with_reg(w, loss, regularizer, train_x, train_y):
-    return calculate_loss(w, loss, train_x, train_y) + regularizer(w)
+def calc_loss_with_reg(w, loss, lambda_val, regularizer, train_x, train_y):
+    return calculate_loss(w, loss, train_x, train_y) + lambda_val*regularizer(w)
 
 
 # Calculate the numerical gradient of func with arguments params.
@@ -90,7 +90,8 @@ def num_gradient(func, params, *args):
 # Perform partial numerical differentiation on func on the index-th dimension
 # with arguments params.
 def part_num_diff(func, params, index, *args):
-    h = 1e-5
+    sqrt_eps = math.sqrt(np.finfo(float).eps)
+    h = sqrt_eps*params[index]
     inc_params = np.copy(params)
     inc_params[index] += h
     return (func(inc_params, *args)-func(params, *args)) / h
@@ -100,5 +101,5 @@ def main():
     return None
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
