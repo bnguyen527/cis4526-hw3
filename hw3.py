479b73c48e58d82f15717a09f1369fd5b23ba4d7
Binh Nguyen <tug21976@temple.edu>
2018-11-18 16:29:38 -0500

    Implement n-fold cross-validation and hyparameters seclection

--- hw3.py
+++ hw3.py
@@ -1,6 +1,14 @@
+import itertools
 import math
 
 import numpy as np
+from scipy import stats
+
+
+ID = 'tug21976'
+LAMBDA_VALUES = [0.1, 0.5, 1, 5, 10]
+LEARN_RATES = [1e-5, 1e-4, 1e-3]
+SQRT_EPS = math.sqrt(np.finfo(float).eps)
 
 
 # Calculate hinge loss given training and prediction label vectors.
@@ -34,8 +42,14 @@ def train_classifier(train_x, train_y, learn_rate, loss, lambda_val=1.0, regular
     train_x_padded = padding(train_x)
     weights = 0.2 * np.random.randn(train_x_padded.shape[1])
     gradient = num_gradient(calc_loss_with_reg, weights, loss, lambda_val, regularizer, train_x, train_y)
+    epoch = 0
+    cost = calc_loss_with_reg(weights, loss, lambda_val, regularizer, train_x, train_y)
+    print('Epoch 0: loss {}'.format(cost))
     while not np.allclose(gradient, np.zeros(weights.size), atol=1e-12):
         weights = weights - learn_rate*gradient
+        epoch += 1
+        cost = calc_loss_with_reg(weights, loss, lambda_val, regularizer, train_x, train_y)
+        print('Epoch {}: loss {}'.format(epoch, cost))
         gradient = num_gradient(calc_loss_with_reg, weights, loss, lambda_val, regularizer, train_x, train_y)
     return weights
 
@@ -54,7 +68,7 @@ def compute_accuracy(test_y, pred_y):
 
 # Return a string representing author's Temple AccessNet.
 def get_id():
-    return 'tug21976'
+    return ID
 
 
 # Detect and remove outliers that are more than k standard deviation from the
@@ -64,6 +78,43 @@ def detect_outliers(train_x, k=6):
         col = train_x[:, i]
         train_x = train_x[np.absolute(col-np.mean(col)) <= k*np.std(col)]
     return train_x
+
+
+# Return a set of accuracies from n-fold cross-validation for a certain set of
+# hyperparameters, loss functions, and regularizers.
+def n_fold_cross_validation(num_folds, train_x, train_y, learn_rate, loss, lambda_val=1.0, regularizer=None):
+    shuffled_indices = np.random.permutation(range(train_x.shape[0]))   # randomize
+    split_indices = np.array_split(range(train_x.shape[0]), num_folds)  # n-fold
+    accuracies = []  # performance over n folds
+    for i in range(num_folds):
+        # Validation set
+        valid_x_split = train_x[shuffled_indices][split_indices[i]]
+        valid_y_split = train_y[shuffled_indices][split_indices[i]]
+        # Train set
+        train_x_split = np.delete(train_x, split_indices[i], 0)
+        train_y_split = np.delete(train_y, split_indices[i])
+        weights = train_classifier(train_x_split, train_y_split, learn_rate, loss, lambda_val, regularizer)
+        pred_y = test_classifier(weights, valid_x_split)
+        accuracies.append(compute_accuracy(valid_y_split, pred_y))
+    return accuracies
+
+
+# Display basic statistics as well as t-test results for an array of possible
+# values of lambda, based on n-fold cross-validation.
+def lambda_selection(num_folds, train_x, train_y, learn_rate, loss, regularizer):
+    lambda_candidates = dict()
+    print('{}-Fold Cross-Validation'.format(num_folds))
+    for lambda_val in LAMBDA_VALUES:
+        accuracies = n_fold_cross_validation(num_folds, train_x, train_y, learn_rate, loss, lambda_val, regularizer)
+        lambda_candidates[lambda_val] = accuracies
+        print('Accuracies for {}: {}'.format(lambda_val, accuracies))
+        # Calculate mean and standard deviation
+        print('Mean: {}\tStandard Deviation: {}'.format(np.mean(accuracies), np.std(accuracies)))
+    # Each combination to compare
+    for lambda1, lambda2 in itertools.combinations(LAMBDA_VALUES, 2):
+        # Only take p-value
+        print('p-value for t-test between {} and {}: {}'.format(lambda1, lambda2, stats.ttest_ind(
+            lambda_candidates[lambda1], lambda_candidates[lambda2], equal_var=False)[1]))
 
 
 # Calculate mean and standard deviation of each column of train_x for testing.
@@ -97,10 +148,10 @@ def calculate_loss(w, func, train_x, train_y):
 # Calculate loss with regularization given vector or linear classifier weights,
 # loss function, regularizer, and training data.
 def calc_loss_with_reg(w, loss, lambda_val, regularizer, train_x, train_y):
-    loss = calculate_loss(w, loss, train_x, train_y)
+    cost = calculate_loss(w, loss, train_x, train_y)
     if regularizer is not None:
-        loss += lambda_val*regularizer(w)
-    return loss
+        cost += lambda_val*regularizer(w)
+    return cost
 
 
 # Calculate the numerical gradient of func with arguments params.
@@ -112,8 +163,7 @@ def num_gradient(func, params, *args):
 # Perform partial numerical differentiation on func on the index-th dimension
 # with arguments params.
 def part_num_diff(func, params, index, *args):
-    sqrt_eps = math.sqrt(np.finfo(float).eps)
-    h = sqrt_eps*params[index]
+    h = SQRT_EPS*params[index]
     inc_params = np.copy(params)
     inc_params[index] += h
     return (func(inc_params, *args)-func(params, *args)) / h
